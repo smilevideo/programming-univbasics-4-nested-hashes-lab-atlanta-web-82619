8e150a2757cbd82ca81bfe8edabc32598de81b62
andrew kim <andrewk1216@gmail.com>
2019-08-20 07:55:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,6 +109,8 @@ def changing_alan
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = alans_new_info
+  
+  return programmer_hash
 end
 
 def adding_to_dennis
@@ -127,5 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  
 end
